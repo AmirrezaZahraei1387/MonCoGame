130c9e3882e0ea9977191eec5e091fb78919b484
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-18 11:13:28 -0600

    a constructor and access functions are created for class Creature

--- BasePlayer/creature.h
+++ BasePlayer/creature.h
@@ -7,15 +7,27 @@
 #include <string_view>
 
 class Creature{
+
 public:
 
+    explicit Creature(std::string_view name,char symbol,int health,int damage,int gold)
+    :m_name{name},
+    m_symbol{symbol}, m_health{health},
+    m_damage{damage}, m_gold{gold}
+    {}
 
+
+    std::string_view getName() const{return m_name;}
+    char getSymbol() const{return m_symbol;}
+    int getHealth() const{return m_health;}
+    int getDamage() const{return m_damage;}
+    int getGold() const{return m_gold;}
 private:
     std::string_view m_name{};
     char m_symbol{};
-    int health{};
-    int damage{};
-    int gold{};
+    int m_health{};
+    int m_damage{};
+    int m_gold{};
 };
 
 #endif //COVV_GME_CREATURE_H
